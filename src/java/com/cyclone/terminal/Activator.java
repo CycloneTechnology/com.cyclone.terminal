7e3f0dcb593255c6d0c99a50a3eb870f45315505
Phil Baxter <phil.baxter@cyclone-technology.com>
2013-06-25 09:24:30 +0000

    RESOLVED - issue NETPREFECT-1951: Cyclone terminal should be OSGified http://dev:8080/browse/NETPREFECT-1951

--- src/java/com/cyclone/terminal/Activator.java
+++ src/java/com/cyclone/terminal/Activator.java
@@ -1,43 +1,18 @@
 package com.cyclone.terminal;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public final class Activator extends AbstractUIPlugin
+public class Activator implements BundleActivator
 {
-    // The shared instance
-    private static Activator s_plugin;
-
-    /**
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-     */
-    @Override
     public void start(BundleContext a_context) throws Exception
     {
-        super.start(a_context);
-        s_plugin = this;
     }
 
-    /**
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
-    @Override
     public void stop(BundleContext a_context) throws Exception
     {
-        s_plugin = null;
-        super.stop(a_context);
-    }
-
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault()
-    {
-        return s_plugin;
     }
 }
