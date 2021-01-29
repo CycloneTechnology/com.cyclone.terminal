6a7f626ae3d6d9056ea65a9649160f6b80c39a43
Phil Baxter <phil.baxter@cyclone-technology.com>
2021-01-29 12:12:15 +0000

    Fix connect address for jms (#40)
    * fix some things for eclipse 4.10
    * first pass
    * Standardise maven folder structure
    * More pom fixes
    * Add intellij files to gitignore
    * ignore unwanted intellij inspection
    * use maven enforcer plugin to specify required maven version
    * Fix test resources
    * reorganise poms to make com.cyclone.terminal self sufficient
    * Fix scala tests that rely on in-package resources
    * update proguard version
    * update logback version
    * remove asm dependency management
    * remove unused package imports
    * refactor VT100 package to vt100 and add README file
    * fixup javadoc warnings
    * Update slf4j to avoid version clashes in gui
    * Allow any javax.annotation version
    * initial switch to servicemix spring bundles from cyclone bundles
    * Remove refs to no longer used spring osgi jars
    * Add missing main resources
    * upgrade netty version
    * Add osx native lib
    * Include -Djava.library.path in surefire path arg line
    * Revert removal of optionals in ssh and ws
    * ensure we use UTF-8 as a source encoding
    * update netty to latest
    * add config for mac build
    * switch spring-instrument dependency to use servicemix bundle
    * Update bouncy castle
    
    Co-authored-by: jeremystone <6453521+jeremystone@users.noreply.github.com>

---
+++ src/main/java/com/cyclone/terminal/emulator/datascope/SimpleDataScopeDialog.java
@@ -0,0 +1,213 @@
+package com.cyclone.terminal.emulator.datascope;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author Phil.Baxter
+ */
+public final class SimpleDataScopeDialog implements DataScope
+{
+    private final Shell dialogShell;
+
+    private final StyledText messageText;
+
+    private final Scope tx;
+
+    private final Scope rx;
+
+    private static final class Scope
+    {
+        private StyledText text;
+
+        private final Direction direction;
+
+        Scope(final Composite a_parent, final Direction a_direction)
+        {
+            direction = a_direction;
+
+            final Composite parentComposite = new Composite(a_parent, SWT.NONE);
+            parentComposite.setLayout(new GridLayout(1, true));
+            parentComposite.setLayoutData(
+                    new GridData(SWT.FILL, SWT.FILL, true, true));
+
+            final Label label = new Label(parentComposite, SWT.NONE);
+            label.setText(a_direction.getDescription());
+
+            text = new StyledText(parentComposite, SWT.MULTI | SWT.BORDER
+                    | SWT.V_SCROLL | SWT.H_SCROLL | SWT.READ_ONLY);
+            text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+            final Font font = new Font(a_parent.getDisplay(), "Courier New", 8,
+                    SWT.NORMAL);
+            text.setFont(font);
+
+            text.addListener(SWT.Modify,
+                    event -> text.setTopIndex(text.getLineCount() - 1));
+        }
+
+        void add(final byte[] a_Data, final int a_count)
+        {
+            if ((a_count > 0) && (!text.isDisposed()))
+            {
+                final StringBuilder sb = new StringBuilder(a_count);
+
+                IntStream.range(0, a_count).forEach(index ->
+                {
+                    sb.append(ASCII_CODES[a_Data[index] & 0xff]);
+                    if (a_Data[index] == 13)
+                    {
+                        sb.append("\r");
+                    }
+                    if (a_Data[index] == 10)
+                    {
+                        sb.append("\n");
+                    }
+                });
+
+                Display.getCurrent().asyncExec(() ->
+                {
+
+                    final int start = text.getCharCount();
+
+                    text.append(sb.toString());
+
+                    final StyleRange styleRange = new StyleRange();
+                    styleRange.start = start;
+                    styleRange.length = sb.length();
+                    styleRange.foreground = direction.getColor();
+                    text.setStyleRange(styleRange);
+                });
+            }
+        }
+    }
+
+    /**
+     * @author Phil.Baxter
+     */
+    public enum Direction
+    {
+        /**
+         *
+         */
+        SEND("Data Sent to Device",
+                Display.getDefault().getSystemColor(SWT.COLOR_BLACK)),
+        /**
+         *
+         */
+        RECEIVE("Data Received from Device",
+                Display.getDefault().getSystemColor(SWT.COLOR_BLACK));
+
+        private final String description;
+
+        private final Color color;
+
+        Direction(final String a_description, final Color a_color)
+        {
+            description = a_description;
+            color = a_color;
+        }
+
+        /**
+         * @return the description
+         */
+        public String getDescription()
+        {
+            return description;
+        }
+
+        /**
+         * @return the color
+         */
+        public Color getColor()
+        {
+            return color;
+        }
+    }
+
+    /**
+     * @param a_parent
+     */
+    public SimpleDataScopeDialog(final Shell a_parent)
+    {
+
+        dialogShell = new Shell(a_parent,
+                SWT.DIALOG_TRIM | SWT.MODELESS | SWT.RESIZE);
+        dialogShell.setLayout(new GridLayout());
+        dialogShell.setText("Terminal Data Scope");
+
+        final Composite parentComposite = new Composite(dialogShell, SWT.NONE);
+        parentComposite.setLayout(new GridLayout(2, true));
+        parentComposite
+                .setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+        tx = new Scope(parentComposite, Direction.SEND);
+        rx = new Scope(parentComposite, Direction.RECEIVE);
+
+        final GridData labelLayoutData = new GridData(SWT.FILL, SWT.FILL, true,
+                false);
+        labelLayoutData.grabExcessHorizontalSpace = true;
+        labelLayoutData.horizontalSpan = 2;
+        final Label label = new Label(parentComposite, SWT.NONE);
+        label.setLayoutData(labelLayoutData);
+        label.setText("Messages");
+
+        final GridData messageLayoutData = new GridData(SWT.FILL, SWT.FILL,
+                true, true);
+        messageLayoutData.grabExcessHorizontalSpace = true;
+        messageLayoutData.horizontalSpan = 2;
+        messageText = new StyledText(parentComposite, SWT.MULTI | SWT.BORDER
+                | SWT.V_SCROLL | SWT.H_SCROLL | SWT.READ_ONLY);
+        messageText.setLayoutData(messageLayoutData);
+
+        messageText.addListener(SWT.Modify, event -> messageText
+                .setTopIndex(messageText.getLineCount() - 1));
+
+        dialogShell.setSize(1024, 500);
+        dialogShell.open();
+    }
+
+    @Override
+    public void close()
+    {
+        dialogShell.close();
+    }
+
+    @Override
+    public void add(final byte[] a_Data, final int a_count,
+            final Direction a_direction)
+    {
+        switch (a_direction)
+        {
+            case RECEIVE:
+                rx.add(a_Data, a_count);
+                break;
+            case SEND:
+                tx.add(a_Data, a_count);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void addMessage(final String a_message)
+    {
+        messageText.getDisplay().asyncExec(() ->
+        {
+            messageText.append(a_message);
+            messageText.append("\n");
+        });
+    }
+}
